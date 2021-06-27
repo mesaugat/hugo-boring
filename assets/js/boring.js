489820a1dce238ee29e75f54c237a29c63945916
Saugat Acharya <mesaugat@gmail.com>
2021-06-27 23:17:04 +0545

    Format file

--- assets/js/boring.js
+++ assets/js/boring.js
@@ -1,62 +1,60 @@
-// Masonry Layout
 function enableMasonry() {
-    window.onload = resizeAllGridItems();
-    window.addEventListener("resize", resizeAllGridItems);
-    var imgs = document.getElementsByTagName('img');
-    for (let img of imgs) {
-        imgLoad(img, resizeAllGridItems())
-    }
+  window.onload = resizeAllGridItems();
+  window.addEventListener('resize', resizeAllGridItems);
+  var imgs = document.getElementsByTagName('img');
+  for (let img of imgs) {
+    imgLoad(img, resizeAllGridItems());
+  }
 }
 
 function imgLoad(img, callback) {
-    var timer = setInterval(function () {
-        if (img.complete) {
-            resizeAllGridItems()
-            clearInterval(timer)
-        }
-    }, 50)
+  var timer = setInterval(function () {
+    if (img.complete) {
+      resizeAllGridItems();
+      clearInterval(timer);
+    }
+  }, 50);
 }
 
 function resizeGridItem(item) {
-    grid = document.getElementsByClassName("masonry")[0];
-    rowHeight = 0;
-    rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
-    rowSpan = Math.ceil((item.querySelector('.grid-content').getBoundingClientRect().height) / rowGap);
-    item.style.gridRowEnd = "span " + rowSpan;
+  grid = document.getElementsByClassName('masonry')[0];
+  rowHeight = 0;
+  rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+  rowSpan = Math.ceil(item.querySelector('.grid-content').getBoundingClientRect().height / rowGap);
+  item.style.gridRowEnd = 'span ' + rowSpan;
 }
 
 function resizeAllGridItems() {
-    allItems = document.getElementsByClassName("item");
-    for (x = 0; x < allItems.length; x++) {
-        resizeGridItem(allItems[x]);
-    }
+  allItems = document.getElementsByClassName('item');
+  for (x = 0; x < allItems.length; x++) {
+    resizeGridItem(allItems[x]);
+  }
 }
 
 function resizeInstance(instance) {
-    item = instance.elements[0];
-    resizeGridItem(item);
+  item = instance.elements[0];
+  resizeGridItem(item);
 }
 
-// switch language
 function switchLanguage() {
-    let element = document.getElementById('languageMode');
-    let targetDiv = document.getElementById('languageOptions');
-    let targets = targetDiv.getElementsByTagName('a')
-    let screen = document.getElementById('is-open-lang');
+  let element = document.getElementById('languageMode');
+  let targetDiv = document.getElementById('languageOptions');
+  let targets = targetDiv.getElementsByTagName('a');
+  let screen = document.getElementById('is-open-lang');
 
-    element.addEventListener('click', () => {
-        targetDiv.classList.toggle('hidden');
-        screen.classList.toggle('hidden')
-    })
+  element.addEventListener('click', () => {
+    targetDiv.classList.toggle('hidden');
+    screen.classList.toggle('hidden');
+  });
 
-    for (let target of targets) {
-        target.addEventListener('click', () => {
-            targetDiv.classList.toggle('hidden')
-            screen.classList.toggle('hidden')
-        })
-    }
-    screen.addEventListener('click', () => {
-        targetDiv.classList.toggle('hidden')
-        screen.classList.toggle('hidden')
-    })
+  for (let target of targets) {
+    target.addEventListener('click', () => {
+      targetDiv.classList.toggle('hidden');
+      screen.classList.toggle('hidden');
+    });
+  }
+  screen.addEventListener('click', () => {
+    targetDiv.classList.toggle('hidden');
+    screen.classList.toggle('hidden');
+  });
 }
